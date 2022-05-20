04bed2f82671cae8e7481a30ab66379b6b3c10f8
larryfab <70698245+larryfab@users.noreply.github.com>
2022-05-20 13:12:28 +0200

    övning
    This reverts commit 3c5d66c1665d461cad86e6ad4479271902f0bc68.

---
+++ src/test/java/com/example/demo/ActorsSchemaTest.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+public class ActorsSchemaTest {
+
+    private SchemaInformation info = new SchemaInformation();
+
+    @Test
+    public void tableHasPrimaryKey() throws SQLException {
+        ColumnsList columns = info.getPrimaryKeys().filterByTableName("actors");
+        Assertions.assertEquals(1,columns.count());
+    }
+}
