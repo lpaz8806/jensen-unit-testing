2d73fd1a673807a657e1a413053056b93fed8b7a
Shanattah <Magnus.taellgaard@hotmail.com>
2022-05-23 14:37:19 +0200

    basic tests

--- src/test/java/com/example/jensen_hr/demo/JobsSchemaTest.java
+++ src/test/java/com/example/jensen_hr/demo/JobsSchemaTest.java
@@ -2,6 +2,7 @@ package com.example.jensen_hr.demo;
 
 import com.example.TableTestBase;
 import com.example.demo.Column;
+import com.example.demo.ColumnType;
 import com.example.demo.ColumnsList;
 import com.example.demo.Config;
 import org.junit.jupiter.api.Assertions;
@@ -24,5 +25,30 @@ public class JobsSchemaTest extends TableTestBase {
     public void primaryKeyIsCalledId() throws SQLException {
         Column column = info.getPrimaryKeys().find("jobs", "id");
         Assertions.assertNotNull(column);
+    }
+    @Test
+    public void nameIsString() throws SQLException {
+        ColumnType column = info.getTypes().findByColumn("jobs", "name");
+        Assertions.assertNotNull(column);
+        Assertions.assertEquals("varchar", column.getType());
+    }
+
+    @Test
+    public void nameIsAtMost60CharactersLong() throws SQLException {
+        ColumnType column = info.getTypes().findByColumn("jobs", "name");
+        Assertions.assertNotNull(column);
+        Assertions.assertEquals(60, column.getMaxLength());
+    }
+    @Test
+    public void minSalaryIsInt() throws SQLException {
+        ColumnType column = info.getTypes().findByColumn("jobs", "min_salary");
+        Assertions.assertNotNull(column);
+        Assertions.assertEquals("int", column.getType());
+    }
+    @Test
+    public void maxSalaryIsInt() throws SQLException {
+        ColumnType column = info.getTypes().findByColumn("jobs", "max_salary");
+        Assertions.assertNotNull(column);
+        Assertions.assertEquals("int", column.getType());
     }
 }
--- src/test/java/com/example/jensen_hr/demo/LocationsSchemaTest.java
+++ src/test/java/com/example/jensen_hr/demo/LocationsSchemaTest.java
@@ -2,6 +2,7 @@ package com.example.jensen_hr.demo;
 
 import com.example.TableTestBase;
 import com.example.demo.Column;
+import com.example.demo.ColumnType;
 import com.example.demo.ColumnsList;
 import com.example.demo.Config;
 import org.junit.jupiter.api.Assertions;
@@ -24,5 +25,47 @@ public class LocationsSchemaTest extends TableTestBase {
     public void primaryKeyIsCalledId() throws SQLException {
         Column column = info.getPrimaryKeys().find("locations", "id");
         Assertions.assertNotNull(column);
+    }
+    @Test
+    public void streetIsAtMost25CharactersLong() throws SQLException {
+        ColumnType column = info.getTypes().findByColumn("locations", "street");
+        Assertions.assertNotNull(column);
+        Assertions.assertEquals(25, column.getMaxLength());
+    }
+    @Test
+    public void zipcodeIsAtMost25CharactersLong() throws SQLException {
+        ColumnType column = info.getTypes().findByColumn("locations", "zipcode");
+        Assertions.assertNotNull(column);
+        Assertions.assertEquals(12, column.getMaxLength());
+    }
+    @Test
+    public void cityIsAtMost30CharactersLong() throws SQLException {
+        ColumnType column = info.getTypes().findByColumn("locations", "city");
+        Assertions.assertNotNull(column);
+        Assertions.assertEquals(30, column.getMaxLength());
+    }
+    @Test
+    public void streetIsString() throws SQLException {
+        ColumnType column = info.getTypes().findByColumn("locations", "street");
+        Assertions.assertNotNull(column);
+        Assertions.assertEquals("varchar", column.getType());
+    }
+    @Test
+    public void zipcodeIsString() throws SQLException {
+        ColumnType column = info.getTypes().findByColumn("locations", "zipcode");
+        Assertions.assertNotNull(column);
+        Assertions.assertEquals("varchar", column.getType());
+    }
+    @Test
+    public void cityIsString() throws SQLException {
+        ColumnType column = info.getTypes().findByColumn("locations", "city");
+        Assertions.assertNotNull(column);
+        Assertions.assertEquals("varchar", column.getType());
+    }
+    @Test
+    public void idCountryIsInt() throws SQLException {
+        ColumnType column = info.getTypes().findByColumn("locations", "id_country");
+        Assertions.assertNotNull(column);
+        Assertions.assertEquals("int", column.getType());
     }
 }
